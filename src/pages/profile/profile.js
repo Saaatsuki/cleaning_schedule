0ca60f3ef825b05d43dea820962d329b6122c4ba
kyumin1227 <kyumin12271227@gmail.com>
2025-04-10 10:14:07 +0900

    Feat: 프로필 기능 추가 및 포맷팅
    - 프로필 이미지 업로드 추가
    - 프로필 토큰 수집 추가
    - CSS 및 JS 파일에서 코드 정리 및 포맷팅

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    try {
-        const mainCodeHTML = `
+document.addEventListener("DOMContentLoaded", function () {
+  try {
+    const mainCodeHTML = `
             <div class="profile-container">
                 <div class="profile-container_header">
                     <img width="48" height="48" src="https://img.icons8.com/fluency-systems-regular/96/FFFFFF/edit-administrator.png" alt="admin-settings-male"/>
@@ -18,14 +18,18 @@ document.addEventListener('DOMContentLoaded', function () {
                         </div>
                     </div>
                     <div class="profile-section">
-                        <div class="pro_name"><h3>${sessionStorage.getItem('familyName') || 'GUEST'} ${sessionStorage.getItem('givenName') || ''}</h3></div>
+                        <div class="pro_name"><h3>${sessionStorage.getItem("familyName") || "GUEST"} ${
+      sessionStorage.getItem("givenName") || ""
+    }</h3></div>
                     </div>
                     <div class="lg_box">
                         <div class="profile-section lg">
                             <div class="lglg stuNum_lglg">
                                 <img src="https://img.icons8.com/pulsar-color/96/student-male.png" alt="student-male"/>
                             </div>
-                            <div class="pro_schNum"><h5>${sessionStorage.getItem('studentNumber') || 'Guest Student'}</h5></div>
+                            <div class="pro_schNum"><h5>${
+                              sessionStorage.getItem("studentNumber") || "Guest Student"
+                            }</h5></div>
                         </div>
                         <div class="profile-section lg">
                             <div class="lglg line_lglg">
@@ -33,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
                             </div>
                             <div class="email-container">
                                 <div class="pro_email">
-                                    <h5 id="email-text">${sessionStorage.getItem('email') || 'example@g.yju.ac.kr'}</h5>
+                                    <h5 id="email-text">${sessionStorage.getItem("email") || "example@g.yju.ac.kr"}</h5>
                                 </div>
                                 <button id="copy-email" class="copy-btn">
                                     <img width="20" height="20" src="https://img.icons8.com/fluency-systems-regular/48/f594b5/copy--v1.png" alt="copy--v1"/>
@@ -112,163 +116,208 @@ document.addEventListener('DOMContentLoaded', function () {
             </div>
         `;
 
-        const mainElement = document.querySelector('main');
-        mainElement.innerHTML += mainCodeHTML;
+    const mainElement = document.querySelector("main");
+    mainElement.innerHTML += mainCodeHTML;
 
+    // 画像アップロードのイベント
+    const imageUpload = document.getElementById("image-upload");
+    const profilePreview = document.getElementById("profile-preview");
+    const uploadTrigger = document.getElementById("upload-trigger");
 
+    if (uploadTrigger && imageUpload) {
+      uploadTrigger.addEventListener("click", function () {
+        imageUpload.click();
+      });
 
-        // 画像アップロードのイベント
-        const imageUpload = document.getElementById("image-upload");
-        const profilePreview = document.getElementById("profile-preview");
-        const uploadTrigger = document.getElementById("upload-trigger");
-
-        if (uploadTrigger && imageUpload) {
-            uploadTrigger.addEventListener("click", function () {
-                imageUpload.click();
-            });
-
-            imageUpload.addEventListener("change", function (event) {
-                const file = event.target.files[0];
-                if (file) {
-                    const reader = new FileReader();
-                    reader.onload = function (e) {
-                        profilePreview.src = e.target.result;
-                        sessionStorage.setItem("profileImage", e.target.result);
-                    };
-                    reader.readAsDataURL(file);
-                    location.reload();
-                }
-            });
-        } else {
-            console.error("要素が見つかりません: #upload-trigger または #image-upload");
+      imageUpload.addEventListener("change", async function (event) {
+        const file = event.target.files[0];
+        if (file) {
+          await updateProfileImage(file);
+          //   const reader = new FileReader();
+          //   reader.onload = function (e) {
+          //     profilePreview.src = e.target.result;
+          //     sessionStorage.setItem("profileImage", e.target.result);
+          //   };
+          //   reader.readAsDataURL(file);
+          sessionStorage.setItem("profileImage", "https://bannote.org/" + file);
+          location.reload();
         }
-
-        
-
-        // ページリロード時に画像を復元
-        const savedProfileImage = sessionStorage.getItem("profileImage");
-        if (savedProfileImage) {
-            profilePreview.src = savedProfileImage;
-        }
-
-        document.getElementById('copy-email').addEventListener('click', function() {
-            const emailText = document.getElementById('email-text').innerText;
-            const tempInput = document.createElement('input');
-            document.body.appendChild(tempInput);
-            tempInput.value = emailText;  
-            tempInput.select();  
-            document.execCommand('copy');
-            document.body.removeChild(tempInput); 
-            alert(`복사 : ${emailText}`); 
-        });
-
-        // プロフィール画像を設定（profileImageがnull, undefined, false, 空文字のいずれかの場合はデフォルト画像を使用）
-        const profileImage = sessionStorage.getItem('profileImage');
-        const imgElement = document.createElement('img');
-        if (!profileImage || profileImage === "false" || profileImage.trim() === "") {
-            imgElement.src = 'https://img.icons8.com/color/96/test-account.png'; 
-        } else {
-            imgElement.src = profileImage;
-        }
-
-        // profile-preview を設定
-        const profileImageDiv = document.getElementById('profile-preview');
-        if (profileImageDiv) {
-            profileImageDiv.innerHTML = ''; 
-            profileImageDiv.appendChild(imgElement);
-        } else {
-            console.error("profile-preview element not found");
-        }
-
-        // toggleMenu 関数
-        function toggleMenu(event, menuClass) {
-            const menu = document.querySelector(menuClass);
-
-            if (!menu) {
-                console.log('Menu not found');
-                return;
-            }
-
-            if (menu.style.display === 'none' || menu.style.display === '') {
-                menu.style.display = 'block';
-                setTimeout(() => {
-                    menu.classList.add('show');
-                }, 10); // showクラスを追加してアニメーション開始
-            } else {
-                menu.classList.remove('show');
-                setTimeout(() => {
-                    menu.style.display = 'none';
-                }, 300); // 非表示にするまでの時間を設定
-            }
-        }
-
-        // LINE 연결をクリックした時に吹き出しの表示・非表示を切り替える
-        document.getElementById('line-toggle')?.addEventListener('click', function(event) {
-            console.log("LINE click");
-            toggleMenu(event, '.line_torisetu');
-        });
-
-        // 吹き出し内の閉じるボタンをクリックした時に吹き出しを閉じる
-        document.querySelector('.torisetu_batsu')?.addEventListener('click', function() {
-            const menu = document.querySelector('.line_torisetu');
-            if (menu) {
-                menu.classList.remove('show');
-                setTimeout(() => {
-                    menu.style.display = 'none';
-                }, 300); // 非表示にするまでの時間を設定
-            }
-            setTimeout(() => {
-                location.reload();
-            }, 500);
-
-        });
-
-        window.copyToken = async function () {
-            try {
-                // セッションストレージからトークンを取得（キー名は "token"）
-                const myToken = sessionStorage.getItem("token");
-                if (!myToken) throw new Error("セッションストレージにトークンが見つかりません");
-        
-                // Bearer 認証付きでリクエスト送信
-                const response = await fetch("https://bannote.org/api/message", {
-                    method: "GET",
-                    headers: {
-                        "Authorization": `Bearer ${myToken}`
-                    }
-                });
-        
-                if (!response.ok) throw new Error("APIからのレスポンスが不正です");
-        
-                const token = await response.text(); // JSONなら response.json()
-        
-                // トークンをクリップボードにコピー
-                await navigator.clipboard.writeText(token);
-        
-                // 成功メッセージ表示
-                document.getElementById("copyTarget").innerText = "복사 완료!";
-            } catch (err) {
-                console.error("トークン取得失敗:", err);
-                document.getElementById("copyTarget").innerText = "복사 실패";
-            }
-        };
-        
-    
-
-        document.getElementById("logout-text").addEventListener("click", function () {
-            // 確認ダイアログを表示
-            let confirmLogout = confirm("로그아웃하셔도 괜찮으시겠어요?");
-
-            // ユーザーが「OK」を押したらログアウト処理
-            if (confirmLogout) {
-                sessionStorage.removeItem("token");  // トークン削除
-                window.location.href = "../login/login.html";  // ログインページへ移動
-            }
-        }); 
-
-
-
-    } catch (error) {
-        console.error('Error loading profile HTML:', error);
+      });
+    } else {
+      console.error("요소를 찾을 수 없습니다: #upload-trigger 또는 #image-upload");
     }
+
+    // ページリロード時に画像を復元
+    const savedProfileImage = sessionStorage.getItem("profileImage");
+    if (savedProfileImage) {
+      profilePreview.src = savedProfileImage;
+    }
+
+    document.getElementById("copy-email").addEventListener("click", function () {
+      const emailText = document.getElementById("email-text").innerText;
+      const tempInput = document.createElement("input");
+      document.body.appendChild(tempInput);
+      tempInput.value = emailText;
+      tempInput.select();
+      document.execCommand("copy");
+      document.body.removeChild(tempInput);
+      alert(`복사 : ${emailText}`);
+    });
+
+    // プロフィール画像を設定（profileImageがnull, undefined, false, 空文字のいずれかの場合はデフォルト画像を使用）
+    const profileImage = sessionStorage.getItem("profileImage");
+    const imgElement = document.createElement("img");
+    if (!profileImage || profileImage === "false" || profileImage.trim() === "") {
+      imgElement.src = "https://img.icons8.com/color/96/test-account.png";
+    } else {
+      imgElement.src = profileImage;
+    }
+
+    // profile-preview を設定
+    const profileImageDiv = document.getElementById("profile-preview");
+    if (profileImageDiv) {
+      profileImageDiv.innerHTML = "";
+      profileImageDiv.appendChild(imgElement);
+    } else {
+      console.error("profile-preview element not found");
+    }
+
+    // toggleMenu 関数
+    function toggleMenu(event, menuClass) {
+      const menu = document.querySelector(menuClass);
+
+      if (!menu) {
+        console.log("Menu not found");
+        return;
+      }
+
+      if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "block";
+        setTimeout(() => {
+          menu.classList.add("show");
+        }, 10); // showクラスを追加してアニメーション開始
+      } else {
+        menu.classList.remove("show");
+        setTimeout(() => {
+          menu.style.display = "none";
+        }, 300); // 非表示にするまでの時間を設定
+      }
+    }
+
+    // LINE 연결をクリックした時に吹き出しの表示・非表示を切り替える
+    document.getElementById("line-toggle")?.addEventListener("click", function (event) {
+      console.log("LINE click");
+      toggleMenu(event, ".line_torisetu");
+    });
+
+    // 吹き出し内の閉じるボタンをクリックした時に吹き出しを閉じる
+    document.querySelector(".torisetu_batsu")?.addEventListener("click", function () {
+      const menu = document.querySelector(".line_torisetu");
+      if (menu) {
+        menu.classList.remove("show");
+        setTimeout(() => {
+          menu.style.display = "none";
+        }, 300); // 非表示にするまでの時間を設定
+      }
+      setTimeout(() => {
+        location.reload();
+      }, 500);
+    });
+
+    window.copyToken = async function () {
+      try {
+        // セッションストレージからトークンを取得（キー名は "token"）
+        const myToken = sessionStorage.getItem("token");
+        if (!myToken) throw new Error("セッションストレージにトークンが見つかりません");
+
+        // Bearer 認証付きでリクエスト送信
+        const response = await fetch("https://bannote.org/api/message", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${myToken}`,
+          },
+        });
+
+        if (!response.ok) throw new Error("APIからのレスポンスが不正です");
+
+        const token = await response.text(); // JSONなら response.json()
+
+        // トークンをクリップボードにコピー
+        await navigator.clipboard.writeText(token);
+
+        // 成功メッセージ表示
+        document.getElementById("copyTarget").innerText = "복사 완료!";
+      } catch (err) {
+        console.error("トークン取得失敗:", err);
+        document.getElementById("copyTarget").innerText = "복사 실패";
+      }
+    };
+
+    document.getElementById("logout-text").addEventListener("click", function () {
+      // 確認ダイアログを表示
+      let confirmLogout = confirm("로그아웃하셔도 괜찮으시겠어요?");
+
+      // ユーザーが「OK」を押したらログアウト処理
+      if (confirmLogout) {
+        sessionStorage.removeItem("token"); // トークン削除
+        window.location.href = "../login/login.html"; // ログインページへ移動
+      }
+    });
+  } catch (error) {
+    console.error("Error loading profile HTML:", error);
+  }
 });
 
+async function copyText() {
+  const accessToken = sessionStorage.getItem("token");
+  // APIからBearer Tokenを取得
+  try {
+    const response = await fetch("https://bannote.org/api/message", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("APIリクエストが失敗しました");
+    }
+
+    const data = await response.json();
+    console.log("API 응답 데이터:", data);
+
+    if (data && data.data) {
+      const token = data.data;
+      let textarea = document.createElement("textarea");
+      textarea.value = token;
+      document.body.appendChild(textarea);
+      textarea.select();
+      document.execCommand("copy");
+      document.body.removeChild(textarea);
+
+      alert("복사 : 정보코드");
+    } else {
+      alert("토큰을 가져오지 못했습니다.");
+    }
+  } catch (error) {
+    console.error("トークンのコピー中にエラーが発生しました:", error);
+  }
+}
+
+async function updateProfileImage(file) {
+  const accessToken = sessionStorage.getItem("token");
+  const formData = new FormData();
+  formData.append("file", file);
+
+  try {
+    const response = await fetch("https://bannote.org/api/members/me/profile-image", {
+      method: "PUT",
+      headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+      body: formData,
+    });
+  } catch (error) {
+    console.error("プロフィール画像の更新に失敗しました:", error);
+  }
+}
